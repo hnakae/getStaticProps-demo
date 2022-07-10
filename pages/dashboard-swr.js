4aa256f322c121794ca8958470f0597566925e36
hnakae <nakae27@gmail.com>
2022-07-10 12:53:33 -0700

    shadow routing

---
+++ pages/dashboard-swr.js
@@ -0,0 +1,26 @@
+import useSwr from "swr";
+
+const fetcher = async () => {
+  const response = await fetch("http://localhost:4000/dashboard");
+  const data = await response.json();
+  return data;
+};
+
+function DashboardSWR() {
+  const { data, error } = useSwr("dashboard", fetcher);
+
+  if (error) return "Failed to load";
+  if (!data) return "loading";
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <h2>Posts - {data.posts}</h2>
+      <h2>Likes - {data.likes}</h2>
+      <h2>Followers - {data.followers}</h2>
+      <h2>Following - {data.following}</h2>
+    </div>
+  );
+}
+
+export default DashboardSWR;
