87e67d3b5e67b899ac03bcdd4da8a550ee790ab4
hnakae <nakae27@gmail.com>
2022-07-10 01:13:20 -0700

    getStaticPaths

--- pages/users.js
+++ pages/users.js
@@ -20,7 +20,7 @@ export default users;
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
-  console.log(data);
+  //   console.log(data);
   return {
     props: {
       users: data,
