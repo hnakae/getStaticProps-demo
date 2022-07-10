ba3e45257aca27224b6ab1efbdcf7dc3f878c591
hnakae <nakae27@gmail.com>
2022-07-09 23:57:48 -0700

    init

---
+++ pages/users.js
@@ -0,0 +1,29 @@
+import User from "../components/user";
+
+const users = ({ users }) => {
+  return (
+    <>
+      <div>users</div>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <User user={user} />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default users;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
